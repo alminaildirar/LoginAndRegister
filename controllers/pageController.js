03f54225ccf3d5de9fb1b08101d5ac4e5fab529c
alminaildirar <alminaildirar@gmail.com>
2022-01-13 16:00:25 +0300

    fixed some bugs

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,9 +1,28 @@
 const res = require("express/lib/response");
 
+const User = require('../models/User')
+
 //http://localhost:3000/...
 
-exports.getIndexPage = (req, res) => {
-    res.status(200).render('index')
+exports.getIndexPage = async (req, res) => {
+
+    if(req.session.userID)
+    {
+        const user = await User.findOne({ _id: req.session.userID})
+        console.log("already login")
+        console.log(user)
+        res.status(200).render('index', {
+            user
+        })
+    }else {
+        const user = null;
+        console.log("not login")
+        console.log(user)
+        res.status(200).render('index', {
+            user
+        })
+    }
+
 }
 
 exports.getLoginPage = (req, res) => {
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,8 +5,10 @@ const checkMiddleware = require('../middlewares/checkMiddleware');
 //http://localhost:3000...
 
 router.route('/').get(pageController.getIndexPage)
+
+//First check, if the user is already logged in, user can not open login or register pages.
 router.route('/login').get(checkMiddleware.alreadyLogin ,pageController.getLoginPage)
-router.route('/register').get(pageController.getRegisterPage)
+router.route('/register').get(checkMiddleware.alreadyLogin,pageController.getRegisterPage)
 
 
 module.exports = router;
